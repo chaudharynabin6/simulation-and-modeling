8599596d9f7c018ec9d233dfe91ae4a8e54b1ac3
chaudharynabin6 <chaudharynabin6@gmail.com>
2022-01-03 23:40:00 +0545

    lab 2: fixing issue and finding random number range for poker test

--- lab2.c
+++ lab2.c
@@ -6,39 +6,46 @@ int main(){
     int  xi,xi_1,xi_2;
     int i;
     
-    int random_number[1000];
-    printf("Enter x0 x1:");
-    scanf("%d %d",&x0,&x1);
+    int random_number[10000];
+    // printf("Enter x0 x1:");
+    // scanf("%d %d",&x0,&x1);
+
+    int total_random_number = 1000;
+    int x;
+    // loop of x is to find the total random number must be supplied to support independence
+    for(x =1;x<total_random_number;x++){
+    
+    x0 = 1254;
+    x1 = 9735;
+    xi_1 = x1;
+    xi_2 = x0;
+    random_number[0] = x0;
+    random_number[1] = x1;
     xi_2 = x0;
     xi_1 = x1;
     random_number[0] = x0;
     random_number[1] = x1;
-    int total_random_number = 1000;
-    for(i=2;i<=total_random_number;i++){
-        random_number[i] = (random_number[i-2] + random_number[i-1]) % m;
-
-        x1 = random_number[i];
-        x0 = random_number[i-1];
-       while(random_number[i] <= 999){
-           random_number[i] = (x0 + x1) % m;
-           x1 = random_number[i];
-           x0 = x1;
-       }
-
-        
-        
+    for(i=2; i<=x; i++){
+        xi_2 = random_number[i-2];
+        xi_1 = random_number[i-1];
+        do{
+        xi = (xi_1 + xi_2) % m;
+        xi_1 = xi_2;
+        xi_2 = xi;
+        }while(0);
+        random_number[i] = xi;
     }
-printf("\n --------------random number generarted ----------------\n");
-    for(i = 0;i<total_random_number;i++){
+// printf("\n --------------random number generarted ----------------\n");
+    for(i = 0;i<x;i++){
 
-        printf("%d  \t",random_number[i]);
+        // printf("%d  \t",random_number[i]);
     }
 
-printf("\n----------------------random number generated ------------------\n");
+// printf("\n----------------------random number generated ------------------\n");
 
 // extracting digit and checking all and counting as well
 int num;
-int digits[5];
+int digits[5] = {0};
 int observed[10] = {0,0,0,0,0,0,0};
 /*
 0 -> all same
@@ -49,10 +56,13 @@ int observed[10] = {0,0,0,0,0,0,0};
 */
 int j,k;
 int temp;
-for(i = 0;i<total_random_number;i++){
+for(i = 0;i<x;i++){
     // extracting digit
     num = random_number[i];
 
+    for(j=0;j<4;j++){
+        digits[j] = 0;
+    }
     j = 0;
     while(num){
         digits[j] = num % 10;
@@ -71,16 +81,16 @@ for(i = 0;i<total_random_number;i++){
         }
     }
     // observed[0] for same digit
-    if((digits[0] == digits[1]) && (digits[1] == digits[2]) && (digits[2] == digits[3])){
+    if((digits[0] == digits[1]) && (digits[1] == digits[2]) && (digits[2] == digits[3]) && (digits[3] == digits[1])){
 
         observed[0]++;
     }
     // for all different
-    else if((digits[0] != digits[1]) && (digits[1] != digits[2]) && (digits[2] != digits[3])){
+    else if((digits[0] != digits[1]) && (digits[0] != digits[2]) && (digits[0] != digits[3])&&(digits[1] != digits[2]) && (digits[1] != digits[3])&& (digits[2] != digits[3])){
         observed[1]++;
     }
     // for three of kind
-    else if(((digits[0] == digits[1])&& (digits[1] == digits[2])) || (digits[1] == digits[2] && digits[2] == digits[3])){
+    else if(((digits[0] == digits[1])&& (digits[1] == digits[2]) && (digits[2] != digits[3]) ) || (  (digits[0] != digits[1]) && digits[1] == digits[2] && digits[2] == digits[3])){
         observed[2]++;
     }
     // for two pair
@@ -108,26 +118,32 @@ for(i = 0;i<total_random_number;i++){
 float test_statistics;
 float sum = 0;
 float expected[5] = {0.001,0.504,0.036,0.027,0.432};
-    printf("\n s.n observed\t expected \t test statistics");
+    // printf("\n s.n observed\t expected \t test statistics");
     for(i=0;i<5;i++){
-        expected[i] = expected[i] * 1000;
+        expected[i] = expected[i] * x;
         test_statistics = observed[i] - expected[i];
         test_statistics = test_statistics*test_statistics / expected[i];
         sum = sum + test_statistics;
 
 
 
-        printf("\n %d \t %d \t %.0f \t %.0f",i+1,observed[i],expected[i],test_statistics);
+        // printf("\n %d \t %d \t %.2f \t %.2f",i+1,observed[i],expected[i],test_statistics);
     }
 
-printf("\n");
+// printf("\n");
 float xai = 9.49;
 
-printf("sum = %f",sum);
+// printf("sum = %f",sum);
 if(sum <= xai){
-    printf("\nindependence\n");
+    printf("\ntotal random_number: %d\n",x);
+    // printf("\nindependence\n");
+    // break;
 }
 else{
-    printf("\n dependent \n");
+    // printf("\n dependent \n");
+    // printf("\n");
+    // printf("%d \t",x);
 }
+
+    }
 }
